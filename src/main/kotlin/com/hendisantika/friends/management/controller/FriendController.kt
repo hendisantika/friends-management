a2b5e4afe6874de42c6bd36e37439329dfa84526
Hendi Santika <hendisantika@gmail.com>
2017-07-12 21:49:14 +0700

    #5 - Added save data

---
+++ src/main/kotlin/com/hendisantika/friends/management/controller/FriendController.kt
@@ -0,0 +1,75 @@
+package com.hendisantika.friends.management.controller
+
+import com.hendisantika.friends.management.domain.Friend
+import com.hendisantika.friends.management.repository.FriendRepo
+import org.apache.log4j.LogManager
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+
+
+
+
+
+/**
+ * Created by hendisantika on 7/12/17.
+ */
+@RestController
+class FriendController @Autowired constructor(val fr: FriendRepo) {
+
+    val log = LogManager.getLogger(FriendController::class.java.name)
+
+    @PostMapping("/api/v1/friends")
+    fun createFriends(@RequestBody friend: Friend?): Map<String, Boolean> {
+        if (friend != null) {
+            fr.save(friend)
+            log.info("Data $friend")
+            log.info("Data Friend save successfully!")
+            return Collections.singletonMap("success", true)
+        } else {
+            log.info("Data NULL")
+            log.info("Data Friend failed!")
+            return Collections.singletonMap("success", false)
+        }
+
+    }
+
+//    @GetMapping("/api/v1/friends")
+//    @ResponseBody
+//    fun getAll():  Map<String, JvmType.Object> {
+//        val data = fr.findAll()
+//        val frMap = mapOf(
+//                "success" to true,
+//                "created" to "created"
+//        )
+//
+//        if (data != null){
+//            log.info("Getting All Friends Data ...")
+//            return frMap
+//        }else{
+//            log.info("Data NULL")
+//            return Collections.singletonMap("success", false)
+//        }
+//
+//    }
+
+    @GetMapping("/api/v1/friends2")
+    @ResponseBody
+    fun listAll(): List<MutableIterable<Friend>> {
+        val data = this.fr.findAll()
+
+        if (data != null){
+            log.info("Getting All Friends Data ...")
+            return listOf(data)
+        }else{
+            log.info("All Friends Data NULL ...")
+            return listOf(data as Nothing)
+        }
+
+    }
+
+    
+
+
+}
--- src/main/kotlin/com/hendisantika/friends/management/domain/Friend.kt
+++ src/main/kotlin/com/hendisantika/friends/management/domain/Friend.kt
@@ -1,10 +1,12 @@
 package com.hendisantika.friends.management.domain
 
 import java.util.*
-
+import javax.persistence.Entity
+import javax.persistence.Id
 
 
 /**
  * Created by hendisantika on 7/12/17.
  */
-data class Friend (var friends: ArrayList<String>? = null)
+@Entity
+data class Friend (@Id var friends: ArrayList<String>? = null)
