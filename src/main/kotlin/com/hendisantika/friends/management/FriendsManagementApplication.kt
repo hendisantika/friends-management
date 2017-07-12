efb72c95ca6c3ea52d6829ee9d241772d2a5fddb
Hendi Santika <hendisantika@gmail.com>
2017-07-12 18:22:31 +0700

    #1 - Initial Commit

---
+++ src/main/kotlin/com/hendisantika/friends/management/FriendsManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.hendisantika.friends.management
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FriendsManagementApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(FriendsManagementApplication::class.java, *args)
+}
