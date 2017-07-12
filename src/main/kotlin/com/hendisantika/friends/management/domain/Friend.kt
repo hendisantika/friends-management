307275a74f3a897304c668136adc442c9b966db7
Hendi Santika <hendisantika@gmail.com>
2017-07-12 19:34:55 +0700

    #3 - Added Friend Model

---
+++ src/main/kotlin/com/hendisantika/friends/management/domain/Friend.kt
@@ -0,0 +1,10 @@
+package com.hendisantika.friends.management.domain
+
+import java.util.*
+
+
+
+/**
+ * Created by hendisantika on 7/12/17.
+ */
+data class Friend (var friends: ArrayList<String>? = null)
